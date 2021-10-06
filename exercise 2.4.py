47d77587b740d6a0b39e582189633365df6467fd
Chronomium <kevinjuanda1@gmail.com>
2021-10-06 17:54:44 +0700

    revised 2.4

--- exercise 2.4.py
+++ exercise 2.4.py
@@ -1,29 +1,29 @@
 price = 99
 
 quant = eval(input('Enter the number of packages purchased: '))
+before = price * quant
 
 if 10 <= quant <= 19:
     disc = 0.1
     discstr = '10%'
-    before = price * quant
+    cut = before * disc
 elif 20 <= quant <= 49:
     disc = 0.2
     discstr = '20%'
-    before = price * quant
+    cut = before * disc
 elif 50 <= quant <= 99:
     disc = 0.3
     discstr = '30%'
-    before = price * quant
+    cut = before * disc
 elif 100 <= quant:
     disc = 0.4
     discstr = '40%'
-    before = price * quant
+    cut = before * disc
 elif quant <= 9:
     disc = 1
     discstr = '0%'
-    before = 0
+    cut = 0
 
-cut = before * disc
 total = before - cut
 
 print(f'Discount amount @ {discstr} : $ {cut}')
